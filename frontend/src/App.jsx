07f58f1a8b762f6aff26647f7465ffef8f018345
knazir <knazir@stanford.edu>
2018-04-10 14:24:25 -0700

    Initial frontend, eslint and base react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={"/img/logo.svg"} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to SeePlusPlus</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
