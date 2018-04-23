9b5260d70c87747a6e9546be6d3cd8370d1d72a9
Ashley Taylor <ataylor4@stanford.edu>
2018-04-23 15:47:42 -0700

    Buttons to visualize information

--- frontend/src/codeEditor/Ide.jsx
+++ frontend/src/codeEditor/Ide.jsx
@@ -6,7 +6,8 @@ import CodeMirrorCard from "../util/CodeMirrorCard";
 class Ide extends Component {
   constructor(props) {
     super(props);
-    this.state = { code: "int main() {\n\tint x = 3;\n\tstring y = \"hello\";\n\treturn 0;\n}" };
+    this.state = { code: "int main() {\n\tint x = 3;\n\tstring y = \"hello\";\n\treturn 0;\n}",
+                   isVisualizing: false };
   }
 
   render() {
@@ -18,18 +19,51 @@ class Ide extends Component {
               options={{
                 mode: "text/x-c++src",
                 indentUnit: 4,
-                lineNumbers: true
+                lineNumbers: true,
+                readOnly: this.state.isVisualizing ? "nocursor" : false
               }}
               value={"int main() {\n\tint x = 3;\n\tstring y = \"hello\";\n\treturn 0;\n}"}
               onChange={(editor, data, value) => {
                 this.setState({ code: value });
               }}
             />
-            <button>Visualize Code</button>
           </div>
         </CodeMirrorCard>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          { this.getButtons() }
+        </div>
       </div>
     );
+  }
+
+  getButtons() {
+    if (this.state.isVisualizing) {
+      return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+          <div style={{ display: "flex" }}>
+            <button>|&lt;</button>
+            <button>&lt;</button>
+            <button>&gt;</button>
+            <button>&gt;|</button>
+          </div>
+          <div>
+            <button onClick={() => this.stopVisualizing()}>Stop Visualization</button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+          <button onClick={() => this.visualizeCode()}>Visualize Code</button>
+      );
+    }
+  }
+
+  visualizeCode() {
+    this.setState({ isVisualizing: true });
+  }
+
+  stopVisualizing() {
+    this.setState({ isVisualizing: false });
   }
 }
 
