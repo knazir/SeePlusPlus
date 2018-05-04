1952c492c810623a49b9c09169ece6e7204c5341
ruechy <wanglucy@stanford.edu>
2018-05-03 22:22:32 -0700

    support stack and heap

--- frontend/src/components/DomCard.jsx
+++ frontend/src/components/DomCard.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import VisualizationTool from "../utils/VisualizationTool";
+import Variable from "../models/Variable";
 
 export default class DomCard extends Component {
   static get propTypes() {
@@ -7,7 +9,9 @@ export default class DomCard extends Component {
       title: PropTypes.string.isRequired,
       color: PropTypes.string.isRequired,
       titleStyle: PropTypes.object,
-      bodyStyle: PropTypes.object
+      bodyStyle: PropTypes.object,
+      splitTitle: PropTypes.bool,
+      title2: PropTypes.string
     };
   }
 
@@ -15,14 +19,40 @@ export default class DomCard extends Component {
     return { titleStyle: {}, bodyStyle: {} };
   }
 
+  getTitle(titleStyle) {
+      if (this.props.splitTitle) {
+        return (
+            <div className="box-title split" style={titleStyle}>
+                <h3 style={{ padding: '0% 0% 0% 25%' }}>{this.props.title}</h3>
+                <h3 style={{ padding: '0% 25% 0% 0%' }}>{this.props.title2}</h3>
+            </div>
+        )
+      } else {
+        return (
+          <div className="box-title" style={titleStyle}>
+            <h3 style={{ padding: 0 }}>{this.props.title}</h3>
+          </div>
+        );
+      }
+  }
+
+  getSplitLine() {
+    let height = this.props.height + 31;
+
+    if (this.props.splitTitle) {
+      return (
+          <div className="split-line" style={{height: height}}/>
+      )
+    }
+  }
+
   render() {
     const titleStyle = Object.assign({}, this.props.titleStyle, { backgroundColor: this.props.color });
     const bodyStyle = Object.assign({}, this.props.bodyStyle, { borderColor: this.props.color });
     return (
       <div className="CodeMirrorCard">
-        <div className="box-title" style={titleStyle}>
-          <h3 style={{ padding: 0 }}>{this.props.title}</h3>
-        </div>
+          {this.getSplitLine()}
+          {this.getTitle(titleStyle)}
         <div className="box-content" style={bodyStyle}>
           {this.props.children}
         </div>
--- frontend/src/visualization/Visualization.jsx
+++ frontend/src/visualization/Visualization.jsx
@@ -5,55 +5,72 @@ import { Layer, Stage } from "react-konva";
 import StackFrameCard from "./StackFrameCard";
 import VariableCard from "./VariableCard";
 import VisualizationTool from "../utils/VisualizationTool";
+import DomCard from "../components/DomCard";
 
 export default class Visualization extends Component {
-  static get propTypes() {
-    return {
-      width: PropTypes.number,
-      height: PropTypes.number,
-      trace: PropTypes.object
-    };
-  }
+    static get propTypes() {
+        return {
+            width: PropTypes.number,
+            height: PropTypes.number,
+            trace: PropTypes.object
+        };
+    }
 
-  getHeapVariableNodes() {
-    const heapVars = this.props.trace.getCurrentStep().getHeapVariables();
-    return heapVars.map(v => {
-      const dimensions = VisualizationTool.getVariableCardDimensions(v);
-      return { ...dimensions, component: <VariableCard key={v.name} variable={v}/> };
-    });
-  }
+    getHeapVariableNodes() {
+        const heapVars = this.props.trace.getCurrentStep().getHeapVariables();
+        return heapVars.map(v => {
+            const dimensions = VisualizationTool.getVariableCardDimensions(v);
+            return {...dimensions, component: <VariableCard key={v.name} variable={v}/>};
+        });
+    }
 
-  getGlobalVariableNodes() {
-    return this.props.trace.getCurrentStep().getGlobalVariables().map(v => {
-      const dimensions = VisualizationTool.getVariableCardDimensions(v);
-      return { ...dimensions, component: <VariableCard key={v.name} variable={v}/> };
-    });
-  }
+    getGlobalVariableNodes() {
+        return this.props.trace.getCurrentStep().getGlobalVariables().map(v => {
+            const dimensions = VisualizationTool.getVariableCardDimensions(v);
+            return {...dimensions, component: <VariableCard key={v.name} variable={v}/>};
+        });
+    }
 
-  getStackFrameNodes() {
-    const currentStep = this.props.trace.getCurrentStep();
-    return currentStep.stack.map(frame => {
-      const active = frame === currentStep.getCurrentStackFrame();
-      const dimensions = VisualizationTool.getStackFrameCardDimensions(frame);
-      return { ...dimensions, component: <StackFrameCard key={frame.toString()} stackFrame={frame} active={active}/> };
-    });
-  }
+    getStackFrameNodes() {
+        const currentStep = this.props.trace.getCurrentStep();
+        return currentStep.stack.map(frame => {
+            const active = frame === currentStep.getCurrentStackFrame();
+            const dimensions = VisualizationTool.getStackFrameCardDimensions(frame);
+            return {
+                ...dimensions,
+                component: <StackFrameCard key={frame.toString()} stackFrame={frame} active={active}/>
+            };
+        });
+    }
 
-  getAllNodes() {
-    const origin = { x: 10, y: 40 };
-    const offset = { x: 0, y: 15 };
-    const nodesToLayout = [...this.getGlobalVariableNodes(), ...this.getStackFrameNodes(), ...this.getHeapVariableNodes()];
-    return VisualizationTool.layoutNodes(nodesToLayout, origin, offset, VisualizationTool.Layouts.COLUMN);
-  }
+    getAllNodes() {
+        const origin = {x: 10, y: 40};
+        const offset = {x: 0, y: 15};
+        const nodesToLayout = [...this.getGlobalVariableNodes(), ...this.getStackFrameNodes(), ...this.getHeapVariableNodes()];
+        return VisualizationTool.layoutNodes(nodesToLayout, origin, offset, VisualizationTool.Layouts.COLUMN);
+    }
 
-  render() {
-    if (!this.props.trace) return <div style={{ width: this.props.width, height: this.props.height }}/>;
-    return (
-      <Stage draggable width={this.props.width} height={this.props.height}>
-        <Layer>
-          {this.getAllNodes()}
-        </Layer>
-      </Stage>
-    );
-  }
+    render() {
+        let height = this.props.height*.87
+        if (!this.props.trace) {
+            return (
+                <div className="visualization" style={{width: this.props.width, height: this.props.height}}>
+                    <DomCard splitTitle={true} height={height} title="Stack" title2="Heap" color="lightgray" bodyStyle={{width: this.props.width, height: height}}/>
+                </div>
+            );
+        } else {
+            return (
+                <div className="visualization" style={{width: this.props.width, height: this.props.height}}>
+                    <DomCard splitTitle={true}  height={height}  title="Stack" title2="Heap" color="lightgray" bodyStyle={{width: this.props.width, height: height}}>
+                        <Stage draggable width={this.props.width} height={this.props.height}>
+                            <Layer>
+                                {this.getAllNodes()}
+                            </Layer>
+                        </Stage>
+
+                    </DomCard>
+                </div>
+            );
+        }
+    }
 }
