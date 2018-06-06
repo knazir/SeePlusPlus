a32ee2f03ece9b5dda7316dab224e6c787f97bbe
Lucy L Wang <wanglucy@stanford.edu>
2018-06-05 21:42:32 -0700

    change 4 to 5 for aesthetics

--- docs/codeSamples/trie.cpp
+++ docs/codeSamples/trie.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 typedef struct TrieNode {
-    TrieNode *elems[4];
+    TrieNode *elems[5];
 } TrieNode;
 
 TrieNode *root = nullptr;
@@ -12,9 +12,9 @@ TrieNode *root = nullptr;
 int main() {
     cout << "yay tries!" << endl;
     root = new TrieNode;
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 5; i++) {
         TrieNode *newNode = new TrieNode;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
         	newNode->elems[i] = nullptr;
         }
         root->elems[i] = newNode;
