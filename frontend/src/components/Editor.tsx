b251fdc83183fc94004bd3670f4ef2f70bdfde49
Kashif Nazir <kashifsnazir@gmail.com>
2025-02-18 07:04:55 -0800

    Add monaco editor and some other UI polish like main file tab

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -1,5 +1,8 @@
 //------------------------------------------------------------------------------
-import React from "react";
+import React, { useState } from "react";
+import { Editor as MonacoEditor } from "@monaco-editor/react";
+
+import cppLogo from "../assets/cpp.png";
 
 import "./Editor.css";
 
@@ -11,9 +14,23 @@ interface EditorProps {
 
 //------------------------------------------------------------------------------
 const Editor: React.FC<EditorProps> = ({ width, height }) => {
+    const [code, setCode] = useState<string>("// Write your code here...");
+
     return (
         <div className="editor" style={{ width, height }}>
-            <textarea className="editorTextArea" placeholder="Write your code here..."/>
+            <div className="tabBar">
+                <div className="fileTab">
+                    <img src={cppLogo} alt="C++ Logo"/>
+                    <code>main.cpp</code>
+                </div>
+            </div>
+            <MonacoEditor width="100%"
+                          height="100%"
+                          defaultLanguage="cpp"
+                          defaultValue={code}
+                          onChange={(value) => setCode(value || "")}
+                          theme="vs-dark"
+            />
         </div>
     );
 }
