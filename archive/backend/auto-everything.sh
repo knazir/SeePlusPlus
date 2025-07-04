5ccee2656e22c7288a2e30c634d34d206276acb2
Kashif Nazir <kashifsnazir@gmail.com>
2025-07-03 22:38:44 -0700

    Move legacy frontend to repo root and delete archive directory

--- archive/backend/auto-everything.sh
+++
@@ -1,2 +0,0 @@
-cd valgrind-3.11.0
-./autogen.sh && ./configure --prefix=`pwd`/inst && make && make install
--- archive/backend/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cd "`dirname "$0"`"
-cp wsgi_backend.py /usr/lib/wsgi-bin
-cp vg_to_opt_trace.py /var/spp/lib
-cp auto_everything.sh /var/spp/lib
